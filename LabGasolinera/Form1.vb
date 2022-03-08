170d02725c625085dd414f63d7a6e4d0c7e91b48
Guzman <Guzman@DESKTOP-29GVUO5>
2022-03-08 09:16:55 -0600

    Agregar archivos de proyecto.

---
+++ LabGasolinera/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a As Double
+        Dim b As Double
+        Dim c As Double
+        Dim d As Double
+
+        a = TextBox1.Text
+        b = a * 35
+        TextBox2.Text = b
+
+
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim c As Double
+        c = TextBox3.Text - TextBox2.Text
+        TextBox4.Text = c
+
+    End Sub
+End Class
